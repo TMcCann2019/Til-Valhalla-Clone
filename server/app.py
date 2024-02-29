814cea59bb0694692258e2661041992ca0b3ebcf
Tim McCann <timothysmccann@gmail.com>
2024-02-29 15:31:32 -0500

    added secret key and a few additional imports

--- server/app.py
+++ server/app.py
@@ -3,16 +3,18 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import abort, jsonify, make_response, request, session
 from flask_restful import Resource
+from werkzeug.exceptions import NotFound, Unauthorized
 
 # Local imports
 from config import app, db, api
 # Add your model imports
+from models import *
 
+app.secret_key = "enter secret key here"
 
 # Views go here!
-
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
