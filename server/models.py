e1ba61792254a8fe696eb08be481bff4bac13037
Tim McCann <timothysmccann@gmail.com>
2024-02-29 17:09:31 -0500

    basic models set

--- server/models.py
+++ server/models.py
@@ -4,3 +4,36 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(10), unique=True, nullable=False)
+    email = db.Column(db.String(25), unique=True, nullable=False)
+    _password = db.Column(db.String(100), nullable=False)
+
+
+class Order(db.Model, SerializerMixin):
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    status = db.Column(db.String)
+    total = db.Column(db.Float)
+
+class OrderItem(db.Model, SerializerMixin):
+    __tablename__ = 'order_items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'))
+    quantity = db.Column(db.Integer)
+    sub_total = db.Column(db.Float)
+
+class Product(db.Model, SerializerMixin):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    price = db.Column(db.Float)
