d7d5dfed0777412fb88cb92d15b999e7db343231
Tim McCann <timothysmccann@gmail.com>
2024-03-01 16:13:11 -0500

    trying to fix bcrypt issues

--- server/config.py
+++ server/config.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_migrate import Migrate
-from flask_restful import Api, Resource
+from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
