7b582a15e6ea909fc3e182011a0915bbb6d91303
Tim McCann <timothysmccann@gmail.com>
2024-03-01 11:48:53 -0500

    started some routes in app.py

--- server/app.py
+++ server/app.py
@@ -17,8 +17,51 @@ app.secret_key = "enter secret key here"
 # Views go here!
 @app.route('/')
 def index():
-    return '<h1>Project Server</h1>'
+    return '<h1>Til Valhalla</h1>'
 
+class Products(Resource):
+    def get(self):
+        products = [p.to_dict() for p in Product.query.all()]
+        response = make_response(products, 200)
+        return response
+    
+    def post(self):
+        data = request.get_json()
+        try:
+            new_product = Product(**data)
+        except ValueError as e:
+            abort(400, e.args[0])
+
+        db.session.add(new_product)
+        db.session.commit()
+        response = make_response(new_product.to_dict(), 201)
+        return response
+    
+api.add_resource(Products, "/products")
+
+class Users(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            new_user = User(**data)
+        except ValueError as e:
+            abort(422, e.args[0])
+
+        db.session.add(new_user)
+        db.session.commit()
+        session['user_id'] = new_user.id
+        response = make_response(new_user.to_dict(), 201)
+        return response
+
+api.add_resource(Users, "/users", "/signup")
+
+@app.route('/login', methods=['POST'])
+def login():
+    user = User.query.filter_by(username=request.get_json()['username']).first()
+    if not user:
+        abort(404, "User not found")
+    session["user_id"] = user.id
+    return make_response(user.to_dict(), 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
