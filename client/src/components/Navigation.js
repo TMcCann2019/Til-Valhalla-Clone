91228b06b7c1399337f6bbc906c674f04dc817a6
Tim McCann <timothysmccann@gmail.com>
2024-03-01 10:22:35 -0500

    Nav component

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import {Link} from 'react-router-dom'
+import styled from 'styled-components'
+import { useHistory } from 'react-router-dom'
+import { GiHamburgerMenu } from 'react-icons/gi'
+
+function Navigation({updateUser}) {
+    const [menu, setMenu] = useState(false)
+    const history = useHistory()
+    const handleLogout = () => {
+    }
+
+    return (
+        <Nav> 
+         <NavH1>Til Valhalla</NavH1>
+         <Menu>
+           {!menu?
+           <div onClick={() => setMenu(!menu)}>
+             <GiHamburgerMenu size={30}/> 
+           </div>:
+           <ul>
+            <li onClick={() => setMenu(!menu)}>x</li>
+            <li><Link to='/product/new'>New Product</Link></li>
+            <li><Link to='/'> Home</Link></li>
+            <li><Link to='/authentication'> Login/Signup</Link></li>
+            <li onClick={handleLogout}> Logout </li>
+           </ul>
+           }
+         </Menu>
+        </Nav>
+    )
+}
+
+export default Navigation
+
+const NavH1 = styled.h1`
+font-family: 'Splash', cursive;
+`
+const Nav = styled.div`
+  display: flex;
+  justify-content:space-between;
+  
+`;
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  a{
+    text-decoration: none;
+    color:black;
+    font-family:Arial;
+  }
+  a:hover{
+    color:green
+  }
+  ul{
+    list-style:none;
+  }
+  
+`;
