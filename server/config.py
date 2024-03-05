192b2344c9b3e1bb823e92759324bb720d7c83b4
Tim McCann <timothysmccann@gmail.com>
2024-03-05 15:24:46 -0500

    added secret key and imports for deploying

--- server/config.py
+++ server/config.py
@@ -1,3 +1,6 @@
+import os
+from dotenv import load_dotenv
+
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
@@ -8,11 +11,11 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URI')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
-app.secret_key = "enter secret key here"
+app.secret_key = "087b0c3a82fb73f4175b1de06c3d718f"
 
 db = SQLAlchemy(engine_options={"echo": True})
 migrate = Migrate(app, db)
