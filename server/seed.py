588472f89e383a799d730389a315d78fc4f590fe
Tim McCann <timothysmccann@gmail.com>
2024-03-05 15:15:37 -0500

    fixed up some seeds to make it look nicer

--- server/seed.py
+++ server/seed.py
@@ -22,122 +22,122 @@ if __name__ == '__main__':
         products = []
 
         p1 = Product(
-            name=fake.name(),
+            name="Let there be military police",
             description=fake.text(),
             image='https://image.spreadshirtmedia.com/image-server/v1/mp/products/T812A2MPA1664PT17X11Y16D13097815S59/views/2,width=800,height=800,appearanceId=2,backgroundColor=F2F2F2,modelId=127,crop=list,version=1560425676,modelImageVersion=1554715238/military-police-military-police-mens-premium-t-shirt.jpg',
             price=randint(1, 30),
             size="Large",
-            color=fake.text()
+            color="black and white"
         )
 
         products.append(p1)
 
         p2 = Product(
-            name=fake.name(),
+            name="stay low, go fast",
             description=fake.text(),
             image='https://ih1.redbubble.net/image.361537736.3970/ssrco,classic_tee,flatlay,101010:01c5ca27c6,front,wide_portrait,750x1000.u2.jpg',
             price=randint(1, 30),
             size="Large",
-            color=fake.text()
+            color="black, gold and white"
         )
 
         products.append(p2)
 
         p3 = Product(
-            name=fake.name(),
+            name="to the soliders that serve",
             description=fake.text(),
             image='https://i.pinimg.com/originals/0c/86/70/0c867045c528b0413b3d8c753aa1d0c7.jpg',
             price=randint(1, 30),
             size="Large",
-            color=fake.text()
+            color="red, white, blue, black"
         )
 
         products.append(p3)
 
         p4 = Product(
-            name=fake.name(),
+            name="Navy",
             description=fake.text(),
             image='https://i.pinimg.com/originals/d2/7b/12/d27b12659a889e3ff2467d42ecbbf816.jpg',
             price=randint(1, 30),
             size="Large",
-            color=fake.text()
+            color="black and goldish yellow"
         )
 
         products.append(p4)
 
         p5 = Product(
-            name=fake.name(),
+            name="Funny Navy",
             description=fake.text(),
             image='https://image.spreadshirtmedia.com/image-server/v1/mp/products/T686A2MPA3026PT17X12Y0D1017862662FS4322/views/1,width=378,height=378,appearanceId=2,backgroundColor=F2F2F2,version=1571718329/funny-navy-design-united-states-navy-army-parody-mens-v-neck-t-shirt.jpg',
             price=randint(1, 30),
             size="Small",
-            color=fake.text()
+            color="black and gold"
         )
 
         products.append(p5)
 
         p6 = Product(
-            name=fake.name(),
+            name="Only Easy day was Yesterday",
             description=fake.text(),
             image='https://i.pinimg.com/originals/ed/44/14/ed44147d57f2805064b604b31669a815.jpg',
             price=randint(1, 30),
             size="Small",
-            color=fake.text()
+            color="black and goldish yellow"
         )
 
         products.append(p6)
 
         p7 = Product(
-            name=fake.name(),
+            name="Some people have unseen battles",
             description=fake.text(),
             image='https://image.spreadshirtmedia.com/image-server/v1/mp/products/T812A2PA1663PT17X12Y62D13350611S77/views/1,width=378,height=378,appearanceId=2,backgroundColor=E8E8E8,version=1524725084/ptsd-result-of-one-s-duty-and-doing-the-task-men-s-premium-t-shirt.jpg',
             price=randint(1, 30),
             size="Small",
-            color=fake.text()
+            color="black and white"
         )
 
         products.append(p7)
 
         p8 = Product(
-            name=fake.name(),
+            name="Long Range shooting",
             description=fake.text(),
             image='https://m.media-amazon.com/images/I/A13usaonutL._CLa%7C2140%2C2000%7C81f1k2mXBvL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_UL1500_.png',
             price=randint(1, 30),
             size="Small",
-            color=fake.text()
+            color="black and white"
         )
 
         products.append(p8)
 
         p9 = Product(
-            name=fake.name(),
+            name="Sniper funny",
             description=fake.text(),
             image='https://i.pinimg.com/736x/c7/9f/7a/c79f7a3a71c7f4cf61677f381c7ab2dd--snipers-funny-tee-shirts.jpg',
             price=randint(1, 30),
             size="Medium",
-            color=fake.text()
+            color="black and white"
         )
 
         products.append(p9)
 
         p10 = Product(
-            name=fake.name(),
+            name="Navy SEALs smile back",
             description=fake.text(),
             image='https://i.pinimg.com/originals/8c/9e/44/8c9e44ffecb5cc9d3f12dc3396915e92.jpg',
             price=randint(1, 30),
             size="Medium",
-            color=fake.text()
+            color="black, gold and white"
         )
 
         products.append(p10)
 
         p11 = Product(
-            name=fake.name(),
+            name="Never quit",
             description=fake.text(),
             image='https://i.pinimg.com/originals/01/c7/6c/01c76c83bf5d36fb17377f1836dc08a5.jpg',
             price=randint(1, 30),
             size="Medium",
-            color=fake.text()
+            color="black and gold"
         )
 
         products.append(p11)
