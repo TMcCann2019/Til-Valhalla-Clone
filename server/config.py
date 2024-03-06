f90ed354d57be36cf343c5f4c2536e34b641c3d7
Tim McCann <timothysmccann@gmail.com>
2024-03-06 10:28:28 -0500

    have backend up and running for deployment

--- server/config.py
+++ server/config.py
@@ -1,12 +1,14 @@
 import os
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
@@ -15,7 +17,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URI')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
-app.secret_key = "087b0c3a82fb73f4175b1de06c3d718f"
+app.secret_key = os.environ.get('SECRET_KEY')
 
 db = SQLAlchemy(engine_options={"echo": True})
 migrate = Migrate(app, db)
